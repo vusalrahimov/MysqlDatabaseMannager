9b6f6b1b00f5057495c8f9497d9001b716780451
vusal <vusalr06@gmail.com>
2022-06-21 15:45:17 +0400

    changes

--- src/databasemanager/Home.java
+++ src/databasemanager/Home.java
@@ -7,6 +7,9 @@ package databasemanager;
 
 import database.DbManager;
 import java.awt.Color;
+import java.awt.Component;
+import java.awt.Image;
+import java.awt.Toolkit;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -16,12 +19,25 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.text.html.HTMLEditorKit;
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.DefaultTreeModel;
 import javax.swing.tree.TreeModel;
 import net.proteanit.sql.DbUtils;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+import sun.misc.IOUtils;
 
 /**
  *
@@ -33,59 +49,62 @@ public class Home extends javax.swing.JFrame {
      * Creates new form Home
      */
     private Connection connection;
+
     public Home() {
         initComponents();
     }
-    
-    public Home(Connection connection){
+
+    public Home(Connection connection) {
         this.connection = connection;
         initComponents();
         setSchema();
         setResultArea();
         setButton();
     }
-    
-    public void setResultArea(){
+
+    public void setResultArea() {
         RESULT_TEXT.setEditable(false);
     }
-    
-    public void setButton(){
+
+
+    public void setButton() {
         RUN_QUERY_BTN.setBackground(Color.GREEN);
         RUN_QUERY_BTN.setOpaque(true);
         RUN_QUERY_BTN.setBorderPainted(false);
         RUN_QUERY_BTN.setForeground(Color.WHITE);
     }
- 
-    public void setSchema(){
+
+    public void setSchema() {
         DefaultMutableTreeNode rootNode = new DefaultMutableTreeNode("Schemas");
         String query = "show schemas";
-        try (PreparedStatement ps = connection.prepareStatement(query);ResultSet rs = ps.executeQuery()){
-            while(rs.next()){
+        try (PreparedStatement ps = connection.prepareStatement(query); ResultSet rs = ps.executeQuery()) {
+            while (rs.next()) {
                 DefaultMutableTreeNode tree = new DefaultMutableTreeNode(rs.getString("Database"));
                 PreparedStatement p = connection.prepareCall("SHOW TABLES FROM " + rs.getString("Database"));
                 ResultSet resultSet = p.executeQuery();
-                while(resultSet.next()){
-                    tree.add(new DefaultMutableTreeNode(resultSet.getString("Tables_in_"+rs.getString("Database"))));
+                while (resultSet.next()) {
+                    tree.add(new DefaultMutableTreeNode(resultSet.getString("Tables_in_" + rs.getString("Database"))));
                 }
                 rootNode.add(tree);
             }
             DefaultTreeModel model = (DefaultTreeModel) SCHEMA_TREE.getModel();
             model.setRoot(rootNode);
             SCHEMA_TREE.setModel(model);
-            
+
         } catch (SQLException ex) {
             Logger.getLogger(Home.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
+
     }
-    
-    public String[] listToArray(List<String> list){
+
+    public String[] listToArray(List<String> list) {
         String[] arr = new String[list.size()];
-        for(int i = 0;i<list.size();i++){
+        for (int i = 0; i < list.size(); i++) {
             arr[i] = list.get(i);
         }
         return arr;
     }
+
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -106,11 +125,12 @@ public class Home extends javax.swing.JFrame {
         SQL_EDITOR = new javax.swing.JEditorPane();
         jScrollPane5 = new javax.swing.JScrollPane();
         RESULT_TABLE = new javax.swing.JTable();
-        jPanel3 = new javax.swing.JPanel();
-        REFRESH_BTN = new javax.swing.JButton();
-        RUN_QUERY_BTN = new javax.swing.JButton();
+        LOADING_PANEL = new javax.swing.JPanel();
+        LOAD_LBL = new javax.swing.JLabel();
         jScrollPane6 = new javax.swing.JScrollPane();
         RESULT_TEXT = new javax.swing.JTextArea();
+        REFRESH_BTN = new javax.swing.JButton();
+        RUN_QUERY_BTN = new javax.swing.JButton();
 
         jMenu1.setText("File");
         jMenuBar1.add(jMenu1);
@@ -127,9 +147,7 @@ public class Home extends javax.swing.JFrame {
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 272, Short.MAX_VALUE))
+            .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 318, Short.MAX_VALUE)
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -155,8 +173,8 @@ public class Home extends javax.swing.JFrame {
         jPanel2.setLayout(jPanel2Layout);
         jPanel2Layout.setHorizontalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jScrollPane5, javax.swing.GroupLayout.DEFAULT_SIZE, 771, Short.MAX_VALUE)
             .addComponent(jScrollPane1)
-            .addComponent(jScrollPane5, javax.swing.GroupLayout.DEFAULT_SIZE, 675, Short.MAX_VALUE)
         );
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -165,6 +183,29 @@ public class Home extends javax.swing.JFrame {
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jScrollPane5, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE))
         );
+
+        javax.swing.GroupLayout LOADING_PANELLayout = new javax.swing.GroupLayout(LOADING_PANEL);
+        LOADING_PANEL.setLayout(LOADING_PANELLayout);
+        LOADING_PANELLayout.setHorizontalGroup(
+            LOADING_PANELLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, LOADING_PANELLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(LOAD_LBL, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        LOADING_PANELLayout.setVerticalGroup(
+            LOADING_PANELLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, LOADING_PANELLayout.createSequentialGroup()
+                .addGap(0, 13, Short.MAX_VALUE)
+                .addComponent(LOAD_LBL))
+        );
+
+        RESULT_TEXT.setBackground(new java.awt.Color(0, 0, 0));
+        RESULT_TEXT.setColumns(20);
+        RESULT_TEXT.setFont(new java.awt.Font("Monospaced", 0, 18)); // NOI18N
+        RESULT_TEXT.setForeground(new java.awt.Color(51, 255, 0));
+        RESULT_TEXT.setLineWrap(true);
+        RESULT_TEXT.setRows(5);
+        jScrollPane6.setViewportView(RESULT_TEXT);
 
         REFRESH_BTN.setFont(new java.awt.Font("Verdana", 0, 14)); // NOI18N
         REFRESH_BTN.setText("Refresh");
@@ -182,55 +223,39 @@ public class Home extends javax.swing.JFrame {
             }
         });
 
-        javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
-        jPanel3.setLayout(jPanel3Layout);
-        jPanel3Layout.setHorizontalGroup(
-            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel3Layout.createSequentialGroup()
-                .addComponent(REFRESH_BTN, javax.swing.GroupLayout.PREFERRED_SIZE, 96, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(RUN_QUERY_BTN, javax.swing.GroupLayout.PREFERRED_SIZE, 113, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-        );
-        jPanel3Layout.setVerticalGroup(
-            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel3Layout.createSequentialGroup()
-                .addContainerGap()
-                .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(REFRESH_BTN, javax.swing.GroupLayout.DEFAULT_SIZE, 44, Short.MAX_VALUE)
-                    .addComponent(RUN_QUERY_BTN, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
-        );
-
-        RESULT_TEXT.setBackground(new java.awt.Color(0, 0, 0));
-        RESULT_TEXT.setColumns(20);
-        RESULT_TEXT.setFont(new java.awt.Font("Monospaced", 0, 18)); // NOI18N
-        RESULT_TEXT.setForeground(new java.awt.Color(51, 255, 0));
-        RESULT_TEXT.setLineWrap(true);
-        RESULT_TEXT.setRows(5);
-        jScrollPane6.setViewportView(RESULT_TEXT);
-
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addGroup(layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addComponent(jPanel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jScrollPane6)
-                    .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                .addContainerGap())
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
+                        .addComponent(REFRESH_BTN, javax.swing.GroupLayout.PREFERRED_SIZE, 96, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(RUN_QUERY_BTN, javax.swing.GroupLayout.PREFERRED_SIZE, 142, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(LOADING_PANEL, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jScrollPane6))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jPanel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jScrollPane6, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE))
+                    .addComponent(jScrollPane6, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addGap(0, 0, Short.MAX_VALUE)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                                .addComponent(RUN_QUERY_BTN, javax.swing.GroupLayout.PREFERRED_SIZE, 45, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addComponent(REFRESH_BTN, javax.swing.GroupLayout.PREFERRED_SIZE, 45, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addComponent(LOADING_PANEL, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
@@ -243,45 +268,53 @@ public class Home extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void REFRESH_BTNActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_REFRESH_BTNActionPerformed
-        setSchema();
+            setSchema();
     }//GEN-LAST:event_REFRESH_BTNActionPerformed
 
     private void RUN_QUERY_BTNActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_RUN_QUERY_BTNActionPerformed
-        String query = SQL_EDITOR.getText();
-        boolean success = executeQuery(query);
-        if(success){
-            RESULT_TEXT.setText("Query executed successfully!");
-            RESULT_TEXT.setForeground(Color.decode("#18AD2F"));
-        }else{
-           RESULT_TEXT.setText("Error occur when query executing!");
-           RESULT_TEXT.setForeground(Color.red); 
-        }
+        try {
+            
+    
+                String query = SQL_EDITOR.getText();
+                boolean success = executeQuery(query);
+                if (success) {
+                    RESULT_TEXT.setText("Query executed successfully!");
+                    RESULT_TEXT.setForeground(Color.decode("#18AD2F"));
+                } else {
+                    RESULT_TEXT.setText("Error occur when query executing!");
+                    RESULT_TEXT.setForeground(Color.red);
+                }
+        }catch(Exception ex){
+            ex.printStackTrace();
+        }   
+           
     }//GEN-LAST:event_RUN_QUERY_BTNActionPerformed
 
-    public boolean executeQuery(String query){
+    
+    public boolean executeQuery(String query) {
         Connection con = DbManager.getConnection();
-        try{
-            if(SCHEMA_TREE.getLastSelectedPathComponent() != null){
+        try {
+            if (SCHEMA_TREE.getLastSelectedPathComponent() != null) {
                 String schema = SCHEMA_TREE.getLastSelectedPathComponent().toString();
                 con = DbManager.getConnection(schema);
             }
             PreparedStatement ps = con.prepareStatement(query);
             ps.execute();
             ResultSet rs = ps.getResultSet();
-            if(rs!=null){
-             setResultTable(rs);   
+            if (rs != null) {
+                setResultTable(rs);
             }
             con.close();
             return true;
-            
-        }catch(Exception ex){
+
+        } catch (Exception ex) {
             ex.printStackTrace();
             return false;
         }
     }
-    
-    public void setResultTable(ResultSet res) throws Exception{
-            RESULT_TABLE.setModel(DbUtils.resultSetToTableModel(res));
+
+    public void setResultTable(ResultSet res) throws Exception {
+        RESULT_TABLE.setModel(DbUtils.resultSetToTableModel(res));
     }
 
     public static void main(String args[]) {
@@ -294,6 +327,8 @@ public class Home extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JPanel LOADING_PANEL;
+    private javax.swing.JLabel LOAD_LBL;
     private javax.swing.JButton REFRESH_BTN;
     private javax.swing.JTable RESULT_TABLE;
     private javax.swing.JTextArea RESULT_TEXT;
@@ -305,7 +340,6 @@ public class Home extends javax.swing.JFrame {
     private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
-    private javax.swing.JPanel jPanel3;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JScrollPane jScrollPane5;
